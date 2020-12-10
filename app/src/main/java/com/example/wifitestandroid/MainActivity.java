00c42d5c437d4b4a11cfec4f90ba80f905c97fbb
Fitsum A <fitsum@gmail.com>
2020-12-11 01:53:50 +0300

    Replace the method

--- app/src/main/java/com/example/wifitestandroid/MainActivity.java
+++ app/src/main/java/com/example/wifitestandroid/MainActivity.java
@@ -3,14 +3,20 @@ package com.example.wifitestandroid;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.net.ConnectivityManager;
+import android.net.Network;
 import android.net.NetworkCapabilities;
 import android.net.NetworkRequest;
+import android.net.wifi.WifiManager;
 import android.net.wifi.WifiNetworkSpecifier;
 import android.net.wifi.WifiNetworkSuggestion;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -30,21 +36,35 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-
     //method that connects to AP
 
     @RequiresApi(api = Build.VERSION_CODES.Q)
     private void connectAP(String ssid, String password) {
 
-        WifiNetworkSpecifier wifiNetworkSpecifier = new WifiNetworkSpecifier.Builder()
-                .setSsid(ssid)
-                .setWpa2Passphrase(password)
-                .build();
-        NetworkRequest networkRequest = new NetworkRequest.Builder()
-                .addTransportType(NetworkCapabilities.TRANSPORT_WIFI)
-                .setNetworkSpecifier(wifiNetworkSpecifier)
-                .build();
-        ConnectivityManager connectivityManager = (ConnectivityManager)this.getApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE);
-        connectivityManager.requestNetwork(networkRequest, new ConnectivityManager.NetworkCallback());
+
+        WifiNetworkSpecifier.Builder builder = new WifiNetworkSpecifier.Builder();
+        builder.setSsid(ssid);
+        builder.setWpa2Passphrase(password);
+
+        WifiNetworkSpecifier wifiNetworkSpecifier = builder.build();
+
+        NetworkRequest.Builder networkRequestBuilder1 = new NetworkRequest.Builder();
+        networkRequestBuilder1.addTransportType(NetworkCapabilities.TRANSPORT_WIFI);
+        networkRequestBuilder1.setNetworkSpecifier(wifiNetworkSpecifier);
+
+        NetworkRequest nr = networkRequestBuilder1.build();
+        ConnectivityManager cm = (ConnectivityManager)
+                this.getSystemService(Context.CONNECTIVITY_SERVICE);
+        ConnectivityManager.NetworkCallback networkCallback = new
+                ConnectivityManager.NetworkCallback() {
+                    @Override
+                    public void onAvailable(Network network) {
+                        super.onAvailable(network);
+                        Log.d("Net", "onAvailable:" + network);
+                        cm.bindProcessToNetwork(network);
+                    }
+                };
+        cm.requestNetwork(nr, networkCallback);
     }
+
 }
